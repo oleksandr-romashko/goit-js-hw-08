c07b4c2dc19b4d20e2d6ed139fafc7c8b297e1cf
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-02-12 18:17:09 +0100

    Fix vimeo player import

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,10 @@
-import 'https://player.vimeo.com/api/player.js';
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const VIDEO_PLAYER_CURRENT_TIME_KEY = 'videoplayer-current-time';
 let currentVideoId;
 
-const player = new Vimeo.Player(document.getElementById('vimeo-player'));
+const player = new Player(document.getElementById('vimeo-player'));
 
 player.on('loaded', onLoaded);
 player.on('timeupdate', throttle(onSaveCurrentTime, 1000, { trailing: false }));
