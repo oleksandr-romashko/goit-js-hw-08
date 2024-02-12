5fdb4feba2fee4b87eb011d2d70cb92752302e0c
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-02-12 20:24:53 +0100

    Fix task-3 solution

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,10 +7,20 @@ const player = new Player('vimeo-player');
 player.setCurrentTime(getPlaybackTime());
 player.on('timeupdate', throttle(onSaveCurrentTime, 1000));
 
+/**
+ * * Saves playback time to Local Storage
+ *
+ * @param {number} {object{seconds}} object with time value
+ */
 function onSaveCurrentTime({ seconds }) {
   localStorage.setItem(PLAYER_CURRENT_TIME_KEY, seconds);
 }
 
+/**
+ * * Loads stored playback time from Local Storage
+ * @returns {number} time value
+ * @returns {number} 0 if no data in local storage
+ */
 function getPlaybackTime() {
   return localStorage.getItem(PLAYER_CURRENT_TIME_KEY) ?? 0;
 }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,21 +3,21 @@ import throttle from 'lodash.throttle';
 const LS_FORM_DATA_KEY = 'feedback-form-state';
 
 const form = document.querySelector('.feedback-form');
-form.addEventListener('input', throttle(onInput, 500));
-form.addEventListener('submit', onFormSubmit);
 
-const storageData = loadFromStorage();
+const storageData = JSON.parse(localStorage.getItem(LS_FORM_DATA_KEY)) ?? {};
 if (Object.keys(storageData).length) {
   [...form.elements].forEach(el => {
     el.value = storageData[el.name];
   });
 }
 
-function onInput() {
-  const formData = getFormData();
-  Object.keys(formData).length ? saveToStorage(formData) : clearStorage();
-}
+form.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('submit', onFormSubmit);
 
+/**
+ * * Retrieves values from form fields.
+ * @returns {object} Feefback form fields data.
+ */
 function getFormData() {
   const formData = {};
   [...form.elements].forEach(({ nodeName, name, value }) => {
@@ -28,6 +28,19 @@ function getFormData() {
   return formData;
 }
 
+/**
+ * * Handles input event and invokes saving form values to local storage.
+ * * If all form fields are emptied by user, clear up local storage.
+ */
+function onInput() {
+  const formData = getFormData();
+  Object.keys(formData).length ? saveToStorage(formData) : clearStorage();
+}
+
+/**
+ * * Handles feedback form submit.
+ * @param {Event} event Occured event.
+ */
 function onFormSubmit(event) {
   event.preventDefault();
   console.log(getFormData());
@@ -35,14 +48,17 @@ function onFormSubmit(event) {
   clearStorage();
 }
 
-function loadFromStorage() {
-  return JSON.parse(localStorage.getItem(LS_FORM_DATA_KEY)) ?? {};
-}
-
+/**
+ * * Saves feedback data to local storage.
+ * @param {object} data data to sage.
+ */
 function saveToStorage(data) {
   localStorage.setItem(LS_FORM_DATA_KEY, JSON.stringify(data));
 }
 
+/**
+ * * Removes feedback data from local storage.
+ */
 function clearStorage() {
   localStorage.removeItem(LS_FORM_DATA_KEY);
 }
